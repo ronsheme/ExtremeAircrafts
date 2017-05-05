195cce37980a18e07971c2b6bf57d68a3ad3f908
Royi Freifeld <freifeld.royi@gmail.com>
2017-05-05 17:23:19 +0300

    Some stuff to annoy Ron >=)

--- src/main/java/akkaLabs/ExtremeAircrafts/Orchestrator.java
+++ src/main/java/akkaLabs/ExtremeAircrafts/Orchestrator.java
@@ -1,50 +1,59 @@
 package akkaLabs.ExtremeAircrafts;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.IntStream;
-
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 
-public class Orchestrator extends AbstractActor {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class Orchestrator extends AbstractActor
+{
 
 	private final LoggingAdapter logger = Logging.getLogger(getContext().getSystem(), this);
-	private Map<Integer,ActorRef> aircraftNumToActor = new HashMap<>();
 	private final Props aircraftProps = Props.create(Aircraft.class);
-	
+	private Map<Integer, ActorRef> aircraftNumToActor = new HashMap<>();
 	private int aircrafts;
 
 	@Override
-	public Receive createReceive() {
-		return receiveBuilder().match(OrchestrationMessage.class, msg -> {
+	public Receive createReceive()
+	{
+		return receiveBuilder().match(OrchestrationMessage.class, msg ->
+		{
 			int n = msg.getNumOfAircrafts();
-			if (aircrafts < n) {//add aircrafts
-				IntStream.range(aircrafts, n).forEach(i -> {
-					logger.info("Creating actor #"+i);
+			if (aircrafts < n)
+			{//add aircrafts
+				IntStream.range(aircrafts, n).forEach(i ->
+				{
+					logger.info("Creating actor #" + i);
 					aircraftNumToActor.put(i, getContext().actorOf(aircraftProps, String.valueOf(i)));
 				});
 			}
-			else if(aircrafts > n){//remove aircrafts
-				IntStream.range(n, aircrafts).forEach(i -> {
-					logger.info("Stopping actor #"+i);
+			else if (aircrafts > n)
+			{//remove aircrafts
+				IntStream.range(n, aircrafts).forEach(i ->
+				{
+					logger.info("Stopping actor #" + i);
 					getContext().stop(aircraftNumToActor.get(i));
 				});
 			}
 		}).build();
 	}
 
-	public static class OrchestrationMessage {
+	public static class OrchestrationMessage
+	{
 		private int numOfAircrafts;
 
-		public OrchestrationMessage(int numOfAircrafts) {
+		public OrchestrationMessage(int numOfAircrafts)
+		{
 			this.numOfAircrafts = numOfAircrafts;
 		}
 
-		public int getNumOfAircrafts() {
+		public int getNumOfAircrafts()
+		{
 			return numOfAircrafts;
 		}
 	}
