505b771323c5aed86165975b242cc857f0427cfc
ron-lenovo <ronsheme@mail.tau.ac.il>
2017-05-30 14:28:48 +0300

    added

---
+++ Engine/src/main/java/akkaLabs/ExtremeAircrafts/Orchestrator.java
@@ -0,0 +1,84 @@
+package akkaLabs.ExtremeAircrafts;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.event.japi.LookupEventBus;
+import akkaLabs.ExtremeAircrafts.commands.aircraft.ChangePosition;
+import akkaLabs.ExtremeAircrafts.commands.aircraft.ModifyAircrafts;
+import akkaLabs.ExtremeAircrafts.eventbus.PositionChangedEvelope;
+import akkaLabs.ExtremeAircrafts.position.Position;
+import com.google.inject.name.Named;
+import org.locationtech.spatial4j.context.SpatialContext;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
+import static akkaLabs.ExtremeAircrafts.ExtremeModule.*;
+
+public class Orchestrator extends AbstractActor {
+	private static final int MIN_SPEED = 10;
+	private static final int MAX_SPEED = 200;
+
+	private final LoggingAdapter logger = Logging.getLogger(getContext().getSystem(), this);
+	private final Map<UUID, ActorRef> uuidToActor;
+	private final SpatialContext spatialContext;
+	private int aircraftsCount;
+	@Named(value="positionChangedEventBus")
+	private LookupEventBus<PositionChangedEvelope, ActorRef, String> eventBus;
+
+	public Orchestrator(SpatialContext spatialContext,LookupEventBus<PositionChangedEvelope, ActorRef, String> eventBus) {
+		this.spatialContext = spatialContext;
+		this.eventBus = eventBus;
+		this.uuidToActor = new HashMap<>();
+	}
+
+	@Override
+	public Receive createReceive() {
+		return receiveBuilder().
+				match(ModifyAircrafts.class, msg -> {
+					int n = msg.getNumOfAircrafts();
+					if (this.aircraftsCount < n)
+					{// add aircrafts
+						IntStream.range(this.aircraftsCount, n).forEach(i ->
+						{
+							UUID uuid = UUID.randomUUID();
+							logger.info("Creating actor #" + i + " uuid:" + uuid.toString());
+							ActorRef newAircraft = getContext().actorOf(getProps(uuid), uuid.toString());
+
+							double latitude = (BOTTOM_RIGHT_LATITUDE - TOP_LEFT_LATITUDE) * Math.random() + TOP_LEFT_LATITUDE;
+							double longitude = (BOTTOM_RIGHT_LONGITUDE - TOP_LEFT_LONGITUDE) * Math.random() + BOTTOM_RIGHT_LONGITUDE;
+							newAircraft.tell(new ChangePosition(new Position(longitude, latitude, 15)), ActorRef.noSender());
+							uuidToActor.put(uuid, newAircraft);
+
+							getContext().watch(newAircraft);
+						});
+					}
+					else if (this.aircraftsCount > n) { // remove aircrafts
+						Iterator<UUID> uuidIterator = uuidToActor.keySet().iterator();
+						IntStream.range(n, this.aircraftsCount).forEach(i -> {
+							UUID uuid = uuidIterator.next();
+							logger.info("Stopping actor with uuid:" + uuid.toString());
+							getContext().stop(uuidToActor.get(uuid));
+						});
+					}
+					this.aircraftsCount = n;
+				}).
+				build();
+	}
+
+	public Props getProps(UUID uuid) {
+		return Props.create(Aircraft.class, () -> new Aircraft(uuid, MIN_SPEED + Math.random() * MAX_SPEED, Math.random() * 360, this.spatialContext,this.eventBus));
+	}
+
+	// for tests
+	public int getAircraftsCount()
+	{
+		return this.aircraftsCount;
+	}
+}
---
+++ Engine/src/main/java/akkaLabs/ExtremeAircrafts/http/HttpUpdater.java
@@ -0,0 +1,52 @@
+package akkaLabs.ExtremeAircrafts.http;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.event.japi.LookupEventBus;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.ContentTypes;
+import akka.http.javadsl.model.HttpEntities;
+import akka.http.javadsl.model.HttpRequest;
+import akka.stream.ActorMaterializer;
+import akka.stream.Materializer;
+import akkaLabs.ExtremeAircrafts.ExtremeModule;
+import akkaLabs.ExtremeAircrafts.eventbus.PositionChangedEvelope;
+import akkaLabs.ExtremeAircrafts.eventbus.PositionChangedEvent;
+import akkaLabs.ExtremeAircrafts.eventbus.PositionChangedEventBus;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by Ron on 20/05/2017.
+ */
+public class HttpUpdater extends AbstractActor {
+
+    private final LoggingAdapter logger = Logging.getLogger(getContext().getSystem(), this);
+
+    private LookupEventBus<PositionChangedEvelope, ActorRef, String> eventBus;
+    private ActorSystem system;
+    private Materializer materializer;
+    private String aircraftAddress;
+    private ObjectMapper mapper = new ObjectMapper();
+
+    public HttpUpdater(LookupEventBus<PositionChangedEvelope, ActorRef, String> eventBus) {
+        this.eventBus = eventBus;
+        this.aircraftAddress = ExtremeModule.AIRCRAFT_SERVER_ADDRESS;
+        this.system = this.getContext().getSystem();
+        this.materializer = ActorMaterializer.create(this.system);
+        this.eventBus.subscribe(this.getSelf(), PositionChangedEventBus.POSITION_CHANGED_TOPIC);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder().match(PositionChangedEvent.class, evt -> {
+            logger.info("Sending HTTP request with PositionChangedEvent for aircraft {}", evt.getAircraftId());
+            Http http = Http.get(system);
+            HttpRequest request = HttpRequest.POST(this.aircraftAddress + "/aircrafts/update").withEntity(HttpEntities.create(ContentTypes.APPLICATION_JSON,
+                mapper.writeValueAsString(new PositionChangedHttpEntity(evt.getAircraftId(), evt.getPosition()))));
+            http.singleRequest(request, materializer);
+        }).build();
+    }
+}
