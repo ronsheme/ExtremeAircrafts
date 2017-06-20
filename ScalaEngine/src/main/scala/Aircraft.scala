ea16b19e804f5d91d630dd45cf9298ab37af24d9
ronsheme <ronsheme@mail.tau.ac.il>
2017-06-20 22:14:12 +0300

    Aircrafts initialized

--- ScalaEngine/src/main/scala/Aircraft.scala
+++ ScalaEngine/src/main/scala/Aircraft.scala
@@ -2,15 +2,16 @@ import java.util.UUID
 
 import akka.actor.{Actor, Props}
 import akka.event.Logging
+import org.locationtech.spatial4j.context.SpatialContext;
 
 /**
   * Created by Ron on 19/06/2017.
   */
-class Aircraft(uuid: UUID) extends Actor  {
+class Aircraft(uuid: UUID, speed: Double, heading: Double, spatialContext: SpatialContext) extends Actor  {
   val log = Logging(context.system, this)
 
   override def receive = {
-    case _      => log.info("Aircraft {}: received unknown message",uuid)
+    case _  => log.info("Aircraft {}: received unknown message",uuid)
   }
 }
 
@@ -21,5 +22,5 @@ object Aircraft {
     * @param uuid The id to be passed to this actor’s constructor.
     * @return a Props for creating this actor
     */
-  def props(uuid: UUID): Props = Props(new Aircraft(uuid))
+  def props(uuid: UUID, speed: Double, heading: Double, spatialContext: SpatialContext): Props = Props(new Aircraft(uuid, speed, heading, spatialContext))
 }
--- ScalaEngine/src/main/scala/Orchestrator.scala
+++ ScalaEngine/src/main/scala/Orchestrator.scala
@@ -2,22 +2,32 @@ import java.util.UUID
 
 import Orchestrator._
 import akka.actor.{Actor, ActorLogging, Props}
+import org.locationtech.spatial4j.context.SpatialContextFactory
+
+import scala.util.Random
 
 /**
   * Created by Ron on 19/06/2017.
   */
 class Orchestrator extends Actor with ActorLogging {
+import Orchestrator.{randomSpeed,randomHeading}
 
   override def receive ={
-    case AddAircraft => {
-      val uuid = UUID.randomUUID();
-      context.actorOf(Aircraft.props(uuid), uuid.toString)
+    case AddAircraft =>
+      val uuid = UUID.randomUUID()
+      context.actorOf(Aircraft.props(uuid,randomSpeed,randomHeading, spatialContext), uuid.toString)
       log.info("{}: created aircraft with uuid: {}",self.path.name,uuid)
-    }
-    case _      => log.info("{}: received unknown message",self.path.name)
+    case _  => log.info("{}: received unknown message",self.path.name)
   }
 }
 
 object Orchestrator{
   case object AddAircraft
+  val random = Random
+  val MIN_SPEED = 10000;//meter/second
+  val MAX_SPEED = 16000;//meter/second
+  val spatialContext = new SpatialContextFactory().newSpatialContext()
+
+  def randomSpeed = MIN_SPEED + random.nextDouble * MAX_SPEED
+  def randomHeading = random.nextDouble * 360
 }
