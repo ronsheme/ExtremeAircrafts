38c3ee3b2e16cbf39ddd7c90a7edec653aef5520
ron-lenovo <ronsheme@mail.tau.ac.il>
2017-07-08 16:27:42 +0300

    rabbitmq publisher

--- ScalaEngine/src/main/scala/rabbit/RabbitPublisher.scala
+++ ScalaEngine/src/main/scala/rabbit/RabbitPublisher.scala
@@ -4,7 +4,7 @@ import java.util.UUID
 
 import akka.actor.Actor
 import akka.event.Logging
-import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.{Channel, Connection, ConnectionFactory}
 
 /**
   * Created by ron-lenovo on 7/8/2017.
@@ -13,10 +13,6 @@ class RabbitPublisher extends Actor{
   import RabbitPublisher._
 
   val log = Logging(context.system,this)
-  val factory = new ConnectionFactory()
-  factory.setHost(HOST)
-  val connection = factory.newConnection()
-  val channel = connection.createChannel()
 
   override def receive: Receive = {
     case (uuid: UUID,position: position.Position,heading: Double) => {
@@ -29,5 +25,18 @@ class RabbitPublisher extends Actor{
 
 object RabbitPublisher{
   val QUEUE_NAME = "hello"
-  val HOST = "my-rabbit"
+  val HOST = "192.168.198.133"//address of vm that hosts the docker
+
+  //sanity check
+  def main(args: Array[String]): Unit = {
+    val factory:ConnectionFactory = new ConnectionFactory()
+    factory.setHost(HOST)
+    val connection:Connection = factory.newConnection()
+    val channel:Channel = connection.createChannel()
+    channel.queueDeclare(QUEUE_NAME,false,false,false,null)
+    val message = "A1"
+    channel.basicPublish("",QUEUE_NAME,null,message.getBytes("UTF-8"))
+    channel.close()
+    connection.close()
+  }
 }
