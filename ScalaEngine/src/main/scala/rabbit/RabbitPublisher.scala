47de0fccce31a487dc8c90fb8b63669225d3c0d7
ron-lenovo <ronsheme@mail.tau.ac.il>
2017-07-08 20:37:00 +0300

    rabbitmq topic use

--- ScalaEngine/src/main/scala/rabbit/RabbitPublisher.scala
+++ ScalaEngine/src/main/scala/rabbit/RabbitPublisher.scala
@@ -14,29 +14,34 @@ class RabbitPublisher extends Actor{
 
   val log = Logging(context.system,this)
 
+  val factory:ConnectionFactory = new ConnectionFactory()
+  factory.setHost(HOST)
+  val connection:Connection = factory.newConnection()
+  val channel:Channel = connection.createChannel()
+  channel.exchangeDeclare(TOPIC_NAME,"fanout")
+  sys.addShutdownHook(close())
+
   override def receive: Receive = {
     case (uuid: UUID,position: position.Position,heading: Double) => {
       log.info(s"Publishing with new position for aircraft $uuid")
-      //TODO continue
+      channel.basicPublish(TOPIC_NAME,"",null,position.toString.getBytes())
+      //TODO set the message to be a json containing the needed values similarly to PositionChangedHttpEntity
     }
     case x => log.info(s"recevied unknown message.$x")
+  }
+
+  def close(): Unit ={
+    channel.close()
+    connection.close()
   }
 }
 
 object RabbitPublisher{
-  val QUEUE_NAME = "hello"
+  val TOPIC_NAME = "position_updates"
   val HOST = "192.168.1.107"//address of vm that hosts the docker
 
   //sanity check
   def main(args: Array[String]): Unit = {
-    val factory:ConnectionFactory = new ConnectionFactory()
-    factory.setHost(HOST)
-    val connection:Connection = factory.newConnection()
-    val channel:Channel = connection.createChannel()
-    channel.queueDeclare(QUEUE_NAME,false,false,false,null)
-    val message = "A1"
-    channel.basicPublish("",QUEUE_NAME,null,message.getBytes("UTF-8"))
-    channel.close()
-    connection.close()
+
   }
 }
